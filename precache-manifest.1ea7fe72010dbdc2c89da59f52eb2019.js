73974475b02749e485b06f7fecaa80d54d3f7c07
Akshay Kumar <akshay.kumar@keyvertex.com>
2020-07-26 18:23:23 +0530

    Updates

---
+++ precache-manifest.1ea7fe72010dbdc2c89da59f52eb2019.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad3317ebc20cf6f3e1bb5a9dfe11177",
+    "url": "/akshay7619/personal-portfolio.git/index.html"
+  },
+  {
+    "revision": "f4d2c6fb8906cfddda2b",
+    "url": "/akshay7619/personal-portfolio.git/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "e5679763fbc9ae44903e",
+    "url": "/akshay7619/personal-portfolio.git/static/css/main.2f735ac1.chunk.css"
+  },
+  {
+    "revision": "f4d2c6fb8906cfddda2b",
+    "url": "/akshay7619/personal-portfolio.git/static/js/2.64db9bb7.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/akshay7619/personal-portfolio.git/static/js/2.64db9bb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5679763fbc9ae44903e",
+    "url": "/akshay7619/personal-portfolio.git/static/js/main.9fd936d9.chunk.js"
+  },
+  {
+    "revision": "99e8dded05afe8c2abcc",
+    "url": "/akshay7619/personal-portfolio.git/static/js/runtime-main.c5826170.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/akshay7619/personal-portfolio.git/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/akshay7619/personal-portfolio.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/akshay7619/personal-portfolio.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/akshay7619/personal-portfolio.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/akshay7619/personal-portfolio.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/akshay7619/personal-portfolio.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/akshay7619/personal-portfolio.git/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/akshay7619/personal-portfolio.git/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/akshay7619/personal-portfolio.git/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/akshay7619/personal-portfolio.git/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/akshay7619/personal-portfolio.git/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "97c2d9a504dabc0ff2f7cefbf685f9ee",
+    "url": "/akshay7619/personal-portfolio.git/static/media/myImage.97c2d9a5.png"
+  }
+]);
