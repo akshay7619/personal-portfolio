f6880930e87892007dda11702e441f26b2ad6bdb
Akshay Kumar <akshay.kumar@keyvertex.com>
2020-07-26 18:07:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React-portfolio
+
+A boilrplate react portfolio to showcase your projects and work
+
+
+Dummy-portfolio link -> https://github.com/akshay7619/personal-portfolio.git
+
+<!-- ![](test.gif) -->
---
+++ src/components/about.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90"
+        },
+        { id: "Angular_skill", content: "Angular", porcentage: "70%", value: "70" },
+        {
+          id: "Node_skill",
+          content: "NodeJS with Express",
+          porcentage: "80%",
+          value: "80"
+        },
+        {
+          id: "ReactJS_skill",
+          content: "ReactJS",
+          porcentage: "60%",
+          value: "60"
+        },
+        {
+          id: "MongoDB_skill",
+          content: "MongoDB",
+          porcentage: "80%",
+          value: "80"
+        },
+        {
+          id: "PostgreSql_skill",
+          content: "PostgreSQL",
+          porcentage: "80%",
+          value: "80"
+        },
+        {
+          id: "Python_skill",
+          content: "Python",
+          porcentage: "60%",
+          value: "60"
+        }
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Hi, I am Akshay Kumar, a Hyderabad based Software Engineer, Energetic Software Engineer with 2 years of experience as a Full Stack Software Engineer in the software industry. An enthusiastic team player and creative thinker. Passionate about learning new technologies and taking responsibility. Strong command over MEAN Stack. "
+        },
+        // {
+        //   id: "second-p-about",
+        //   content:
+        //     "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+        // },
+        // {
+        //   id: "third-p-about",
+        //   content:
+        //     "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+        // }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map(skill => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map(content => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
